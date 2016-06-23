f909be22f54021004421ed5cd2b9d35b2b3ddf60
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2016-06-22 21:18:03 -0400

    Improvements to template:
    Ability to save and restore context using "with" sections:
    
        {% set a = 100 %}
        {% with %}
            {% set a = 200 %}
            {{ a }}
        {% endwith %}
        {{ a }}
    
    Ability to include another file for a template at run time. The included
    file will receive a copy of the current context.  Currently there is no
    checking on the filename other than ensuring it is a string.
    
        {% include path/to/file.tmpl %}
    
    Line numbers are reported in syntax errors.

--- python/mrbavii/template.py
+++ python/mrbavii/template.py
@@ -104,7 +104,7 @@ class CodeBuilder(object):
 
         return names
 
-        
+
 class Template(object):
     """ Simple template parser and renderer.
 
@@ -158,128 +158,146 @@ class Template(object):
 
     def __init__(self, text=None, filename=None, *contexts):
         """ Initialize a template with context variables. """
+        
+        # Initialize
+        self._filename = filename
 
-        if text is None and filename is None:
-            sys.exit(-1)
-            # TODO: error
-
-        if not text:
-            self._filename = filename
-            text = open(filename, "rU").read()
-        else:
-            self._filename = None
+        if text is None:
+            if self._filename is None:
+                raise Error("Filename must be specified if text is not.")
+            text = open(self._filename, "rU").read()
 
         # Copy all contexts into our context
         self._context = {}
         for context in contexts:
             self._context.update(context)
 
-        # Keep track of variables accessed
-        self._all_vars = set()
-        self._loop_vars = set()
+        # Stack and line number
+        self._ops_stack = []
+        self._line = 0
 
         # Manage the code we are building.
         code = CodeBuilder()
 
-        code.add_line("def render_function(context, do_dots):")
+        code.add_line("def render_function(owner, context, outresult=None):")
         code.indent()
-        vars_code = code.add_section()
+
+        code.add_line("if outresult is None:")
+        code.indent()
         code.add_line("result = []")
+        code.dedent()
+        code.add_line("else:")
+        code.indent()
+        code.add_line("result = outresult")
+        code.dedent()
+
+        code.add_line("saved_contexts = []")
         code.add_line("append_result = result.append")
         code.add_line("extend_result = result.extend")
         code.add_line("to_str = str")
         body_code = code.add_section()
+        
+        code.add_line("if outresult is None:")
+        code.indent()
         code.add_line("return ''.join(result)")
+        code.dedent()
+        
         code.dedent()
 
         self._build_code(text, body_code)
 
-        for var_name in self._all_vars - self._loop_vars:
-            vars_code.add_line("c_{0} = context['{0}']".format(var_name))
-
-
+        # Extract the render function
         self._render_function = code.get_globals()["render_function"]
 
     def _build_code(self, text, code):
         """ Build the code for the template. """
 
-        # Whitespace control function
-        def whitespace_control(token):
-            if token[2:3] == "-":
-                code.add_line("if len(result) > 0:")
-                code.indent()
-                code.add_line("last_nl = result[-1].rfind('\\n')")
-                code.add_line("if last_nl == -1:")
-                code.indent()
-                code.add_line("result[-1] = result[-1].rstrip()")
-                code.dedent()
-                code.add_line("else:")
-                code.indent()
-                code.add_line("result[-1] = result[-1][:last_nl] + result[-1][last_nl:].rstrip()")
-                code.dedent()
-                code.dedent()
-                return token[3:-2].strip()
-            else:
-                return token[2:-2].strip()
-
-
-        # Keep track of our stack
-        ops_stack = []
-
         # Split tokens
-        tokens = re.split(r"(?s)({{.*?}}|{%.*?%}|{#.*?#})", text)
+        for linetext in text.splitlines():
+            if self._line > 0:
+                code.add_line("append_result('\\n')")
+            self._line += 1
+            self._build_line(linetext, code)
 
-        for token in tokens:
+        if self._ops_stack:
+            self._syntax_error(
+                "Unmatched action tag", 
+                self.ops_stack[-1][0],
+                self._ops_stack[-1][1]
+            )
+
+
+    def _build_line(self, text, code):
+        """ Build code from a single line. """
+
+        for token in re.split(r"(?s)({{.*?}}|{%.*?%}|{#.*?#})", text):
+
             if token.startswith("{#"):
                 # Just a comment
-                whitespace_control(token)
+                self._whitespace_control(token, code)
                 continue
 
             elif token.startswith("{{"):
                 # Output some value
-                token = whitespace_control(token)
+                token = self._whitespace_control(token, code)
 
-                expr = self._expr_code(token)
+                expr = self._prep_expr([token])
                 code.add_line("append_result(to_str({0}))".format(expr))
             
             elif token.startswith("{%"):
                 # An action
-                token = whitespace_control(token)
+                token = self._whitespace_control(token, code)
                 
                 words = token.split()
 
-                if words[0] == "set":
+                if words[0] == "include":
+                    # include <filename>
+                    if len(words) != 2:
+                        self._syntax_error("Don't understand include", token, self._line)
+
+                    filename = words[1] # TODO: CHECK THIS FOR FILENAME SANITY
+                    code.add_line("owner._include({0}, context, result)".format(repr(filename)))
+                
+                elif words[0] == "set":
                     # set <variable> = <condition...>
                     if len(words) < 4 or words[2] != "=":
-                        self._syntax_error("Don't understand set", token)
+                        self._syntax_error("Don't understand set", token, self._line)
 
-                    self._variable(words[1], None)
-                    code.add_line("c_{0} = {1}".format(
-                            words[1],
-                            self._expr_code(" ".join(words[3:])),
+                    self._variable(words[1])
+                    code.add_line("context[{0}] = {1}".format(
+                            repr(words[1]),
+                            self._prep_expr(words[3:]),
                         )
                     )
+
+                elif words[0] == "with":
+                    # with
+                    if len(words) != 1:
+                        self._syntax_error("Don't understand with", token, self._line)
+                    self._ops_stack.append(["with", self._line])
+                    code.add_line("saved_contexts.append(dict(context))")
+
                 elif words[0] == "if":
                     # if <condition...>
                     if len(words) < 2:
-                        self._syntax_error("Don't understand if", token)
-                    ops_stack.append("if")
-                    expr = self._expr_code(" ".join(words[1:]))
+                        self._syntax_error("Don't understand if", token, self._line)
+                    self._ops_stack.append(["if", self._line])
+                    expr = self._prep_expr(words[1:])
                     code.add_line("if {0}:".format(expr))
                     code.indent()
 
                 elif words[0] == "elif":
                     # elif <condition...>
                     if len(words) < 2:
-                        self._syntax_error("Don't understand elif", token)
+                        self._syntax_error("Don't understand elif", token, self._line)
 
-                    if not ops_stack:
-                        self._syntax_error("Mismatched elif", token)
-                    start_what = ops_stack[-1]
-                    if start_what != "if":
-                        self._syntax_error("Mismatched elif", token)
+                    if not self._ops_stack:
+                        self._syntax_error("Mismatched elif", token, self._line)
+                    start_what = self._ops_stack[-1]
+                    if start_what[0] != "if":
+                        self._syntax_error("Mismatched elif", token, self._line)
 
-                    expr = self._expr_code(" ".join(words[1:]))
+                    expr = self._prep_expr(words[1:])
                     code.dedent()
                     code.add_line("elif {0}:".format(expr))
                     code.indent()
@@ -287,13 +305,13 @@ class Template(object):
                 elif words[0] == "else":
                     # else
                     if len(words) != 1:
-                        self._syntax_error("Don't understand else", token)
+                        self._syntax_error("Don't understand else", token, self._line)
 
-                    if not ops_stack:
-                        self._syntax_error("Mismatched else", token)
-                    start_what = ops_stack[-1]
-                    if start_what != "if":
-                        self._syntax_error("Mismatched else", token)
+                    if not self._ops_stack:
+                        self._syntax_error("Mismatched else", token, self._line)
+                    start_what = self._ops_stack[-1]
+                    if start_what[0] != "if":
+                        self._syntax_error("Mismatched else", token, self._line)
 
                     code.dedent()
                     code.add_line("else:")
@@ -302,42 +320,66 @@ class Template(object):
                 elif words[0] == "for":
                     # for <variable> in <condition...>
                     if len(words) < 4 or words[2] != "in":
-                        self._syntax_error("Don't understarnd for", token)
-                    ops_stack.append("for")
-                    self._variable(words[1], self._loop_vars)
+                        self._syntax_error("Don't understarnd for", token, self._line)
+                    self._ops_stack.append(["for", self._line])
+                    self._variable(words[1])
                     code.add_line(
-                        "for c_{0} in {1}:".format(
-                            words[1],
-                            self._expr_code(" ".join(words[3:]))
+                        "for context[{0}] in {1}:".format(
+                            repr(words[1]),
+                            self._prep_expr(words[3:])
                         )
                     )
                     code.indent()
 
                 elif words[0].startswith("end"):
                     if len(words) != 1:
-                        self._syntax_error("Don't understand end", token)
+                        self._syntax_error("Don't understand end", token, self._line)
 
                     end_what = words[0][3:]
-                    if not ops_stack:
-                        self._syntax_error("Too many ends", token)
-                    start_what = ops_stack.pop()
-                    if start_what != end_what:
-                        self._syntax_error("Mismatched end tag", end_what)
-                    code.dedent()
+                    if not self._ops_stack:
+                        self._syntax_error("Too many ends", token, self._line)
+                    start_what = self._ops_stack.pop()
+                    if start_what[0] != end_what:
+                        self._syntax_error("Mismatched end tag", end_what, self._line)
+
+                    if end_what == "with":
+                        code.add_line("context = saved_contexts.pop()")
+                    else:
+                        code.dedent()
                 else:
-                    self._syntax_error("Don't understand tag", words[0])
+                    self._syntax_error("Don't understand tag", words[0], self._line)
 
             else:
                 #Literal content
                 if token:
                     code.add_line("append_result(to_str({0}))".format(repr(token)))
 
-        if ops_stack:
-            self._syntax_error("Unmatched action tag", ops_stack[-1])
+    def _whitespace_control(self, token, code):
+        if token[2:3] == "-":
+            code.add_line("if len(result) > 0:")
+            code.indent()
+            code.add_line("last_nl = result[-1].rfind('\\n')")
+            code.add_line("if last_nl == -1:")
+            code.indent()
+            code.add_line("result[-1] = result[-1].rstrip()")
+            code.dedent()
+            code.add_line("else:")
+            code.indent()
+            code.add_line("result[-1] = result[-1][:last_nl] + result[-1][last_nl:].rstrip()")
+            code.dedent()
+            code.dedent()
+            return token[3:-2].strip()
+        else:
+            return token[2:-2].strip()
+
+    def _prep_expr(self, expr):
+        """ Prepare an expression by stripping whitespace and then parsing it """
+        # Join all expressions, but each one may have whitespace so split it
+        # into list with no whitespaces, then join them all together
+        return self._expr_code("".join("".join(expr).split()))
 
     def _expr_code(self, expr):
         """ Create a python expression for output, if, and for. """
-        expr = expr.strip()
 
         if len(expr) == 0:
             code = repr("")
@@ -346,28 +388,28 @@ class Template(object):
             code = self._expr_code(pipes[0])
             for pipe in pipes[1:]:
                 (func, params) = self._parse_pipe(pipe)
-                self._variable(func, self._all_vars)
+                self._variable(func)
 
                 params_code = []
                 for p in params:
-                    params_code.append(self._expr_code(p.strip()))
+                    params_code.append(self._expr_code(p))
                 if params_code:
-                    code = "c_{0}({1},{2})".format(func, code, ",".join(params_code))
+                    code = "context[{0}]({1},{2})".format(repr(func), code, ",".join(params_code))
                 else:
-                    code = "c_{0}({1})".format(func, code)
+                    code = "context[{0}]({1})".format(repr(func), code)
 
         elif "." in expr:
             dots = expr.split(".")
             code = self._expr_code(dots[0])
             args = ", ".join(repr(d) for d in dots[1:])
-            code = "do_dots({0}, {1})".format(code, args)
+            code = "owner._do_dots({0}, {1})".format(code, args)
 
         elif self._isint(expr):
             code = repr(expr)
 
         else:
-            self._variable(expr, self._all_vars)
-            code = "c_{0}".format(expr)
+            self._variable(expr)
+            code = "context[{0}]".format(repr(expr))
 
         return code
 
@@ -379,25 +421,25 @@ class Template(object):
         except ValueError as e:
             return False
 
-    def _syntax_error(self, msg, thing):
+    def _syntax_error(self, msg, thing, where):
         """ Raise an error if something is wrong. """
 
-        raise Error("{0}: {1}".format(msg, repr(thing)))
+        if self._filename:
+            raise Error("{0} on line {1} file {2}: - {3}".format(msg, where, self._filename, repr(thing)))
+        else:
+            raise Error("{0} on line {1}: - {2}".format(msg, where, repr(thing)))
 
-    def _variable(self, what, where):
+    def _variable(self, what):
         """ Track a varialbe that is used. """
         if not re.match(r"[_a-zA-Z][_a-zA-Z0-9]*$", what):
-            self._syntax_error("Not a valid name", what)
+            self._syntax_error("Not a valid name", what, self._line)
     
-        if not where is None:
-            where.add(what)
-
-    def render(self, context=None):
+    def render(self, context=None, result=None):
         """ Render teh template. """
-        render_context= dict(self._context)
+        render_context = dict(self._context)
         if context:
             render_context.update(context)
-        return self._render_function(render_context, self._do_dots)
+        return self._render_function(self, render_context, result)
 
     def _do_dots(self, value, *dots):
         """ Evaluate dotted expressions. """
@@ -421,14 +463,25 @@ class Template(object):
         func = pipe[0:start]
         end = pipe.rfind(")")
         if end != len(pipe) - 1:
-            raise self._syntax_error("Don't understand pipe", pipe)
+            raise self._syntax_error("Don't understand pipe", pipe, self._line)
 
         params = pipe[start + 1:end].split(",")
 
-        return (func.strip(), params)
-            
-                    
+        return (func, params)
+
+    def _include(self, filename, context, result):
+        """ Include another template. """
+        if self._filename is None:
+            raise Error("Can't include a template if a filename isn't specified.")
+
+        newfile = os.path.join(os.path.dirname(self._filename), *(filename.split("/")))
+        t = Template(filename=newfile)
+        t.render(context, result)
+
+
+
 # Test
 ################################################################################
 
 # TODO
+
