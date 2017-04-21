2e336ed86750690e0b73a018637b1d446ea41657
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2017-04-20 21:33:45 -0400

    Bumped version

--- mrbavii_lib_template/_version.py
+++ mrbavii_lib_template/_version.py
@@ -1,6 +1,6 @@
 """ A collection of reusable code modules. """
 
-__version__     = "0.1.8"
+__version__     = "0.1.9"
 __author__      = "Brian Allen Vanderburg II"
 __copyright__   = "Copyright 2016-2017"
 __license__     = "Apache License 2.0"
