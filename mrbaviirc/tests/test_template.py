af30464571a009c10698febe4171f05eddfca97b
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2018-01-27 15:35:57 -0500

    Now using pytest for testing.

---
+++ mrbaviirc/tests/test_template.py
@@ -0,0 +1,109 @@
+""" pytest-capable tests for the template engine. """
+
+__author__      = "Brian Allen Vanderburg II"
+__copyright__   = "Copyright 2016"
+__license__     = "Apache License 2.0"
+
+
+import os
+import json
+import glob
+
+from ..template import UnrestrictedLoader, SearchPathLoader, Environment, StdLib, StringRenderer
+
+DATADIR = os.path.join(os.path.dirname(__file__), "template_data")
+
+
+# setup
+def test_compare_unrestricted_loader():
+    loader = UnrestrictedLoader()
+    env = Environment({"lib": StdLib() }, loader=loader)
+    env.enable_code()
+
+    do_test_compare(env, False)
+
+def test_compare_search_path_loader():
+    loader = SearchPathLoader(DATADIR)
+    env = Environment({"lib": StdLib() }, loader=loader)
+    env.enable_code()
+
+    do_test_compare(env, True)
+
+def do_test_compare(env, search_path_loader):
+    """ Run tests by applying template to input and comparing output. """
+
+    with open(os.path.join(DATADIR, "data.json"), "rU") as handle:
+        data = json.load(handle)
+
+    for path in sorted(glob.glob(os.path.join(DATADIR, "*.tmpl"))):
+        source = path if not search_path_loader else os.path.basename(path)
+
+        tmpl = env.load_file(filename=source)
+        rndr = StringRenderer()
+        tmpl.render(rndr, data)
+
+        target = path[:-5] + ".txt"
+        contents = rndr.get()
+
+        with open(target, "rU") as handle:
+            target_contents = handle.read()
+
+        assert contents == target_contents
+        #self.assertEqual(
+        #    contents,
+        #    target_contents,
+        #    "render compare failed: {0}".format(os.path.basename(path))
+        #)
+
+        sections = rndr.get_sections()
+        for section in sections:
+            target = path[:-5] + "_" + section + ".txt"
+            contents = rndr.get_section(section)
+
+            with open(target, "rU") as handle:
+                target_contents = handle.read()
+
+            assert contents == target_contents
+            #self.assertEqual(
+            #    contents,
+            #    target_contents,
+            #    "render comapre failed: {0}:{1}".format(
+            #        os.path.basename(path),
+            #        section
+            #))
+
+def test_search_path():
+    """ Test the search path loader. """
+    paths = [
+        os.path.join(DATADIR, "searchpath/1"),
+        os.path.join(DATADIR, "searchpath/2"),
+        os.path.join(DATADIR, "searchpath/3")
+    ]
+
+    target = os.path.join(DATADIR, "searchpath/output.txt")
+    
+    loader=SearchPathLoader(paths)
+    env = Environment({"lib": StdLib()}, loader=loader)
+
+
+    with open(os.path.join(DATADIR, "data.json"), "rU") as handle:
+        data = json.load(handle)
+
+    tmpl = env.load_file("/main.tmpl")
+    rndr = StringRenderer()
+    tmpl.render(rndr, data)
+
+    contents = rndr.get()
+
+    with open(target, "rU") as handle:
+        target_contents = handle.read()
+
+    assert contents == target_contents
+    #self.assertEqual(
+    #    contents,
+    #    target_contents,
+    #    "render compare failed during search path test"
+    #)
+
+
+
