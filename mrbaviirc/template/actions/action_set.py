2901f3e43958a5d4f76d0a297b407b5007e3bb08
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2022-07-04 18:42:43 -0400

    Added else segment to set tag

--- mrbaviirc/template/actions/action_set.py
+++ mrbaviirc/template/actions/action_set.py
@@ -9,20 +9,35 @@ __license__ = "Apache License 2.0"
 
 from . import ActionHandler
 from ..nodes import Node
+from ..errors import Error
+from ..tokenizer import Token
 
 
 class AssignNode(Node):
     """ Set a variable to a subvariable. """
 
-    def __init__(self, template, line, assigns):
+    def __init__(self, template, line, assigns, elses):
         """ Initialize. """
         Node.__init__(self, template, line)
         self.assigns = assigns
+        self.elses = elses
 
     def render(self, state):
         """ Set the value. """
-        for (var, expr) in self.assigns:
-            state.set_var(var[0], expr.eval(state), var[1])
+
+        # An exception from assigns only passes up if there are no
+        # thens and no elses expressions
+
+        try:
+            for (var, expr) in self.assigns:
+                state.set_var(var[0], expr.eval(state), var[1])
+        except Error as e:
+            # An expression error occurred, run elses or reraise error
+            if self.elses is not None:
+                for (var, expr) in self.elses:
+                    state.set_var(var[0], expr.eval(state), var[1])
+            else:
+                raise
 
 
 class SetActionHandler(ActionHandler):
@@ -30,10 +45,43 @@ class SetActionHandler(ActionHandler):
 
     def handle_action_set(self, line, start, end):
         """ Handle set. """
-        assigns = self.parser.parse_multi_assign(start, end, allow_type=True)
 
-        node = AssignNode(self.template, line, assigns)
+        parser = self.parser
+        segments = parser.find_tag_segments(start, end)
+        assigns = None
+        elses = None
+
+        # Normal assignments are first
+        if len(segments) > 0:
+            (start, end) = segments[0]
+            assigns = self.parser.parse_multi_assign(start, end, allow_type=True)
+
+        # Find then/else assignments
+        for segment in segments[1:]:
+            (start, end) = segment
+
+            token = parser.get_expected_token(
+                start,
+                end,
+                Token.TYPE_WORD,
+                values=["else"]
+            )
+            start += 1
+
+            if token.value == "else":
+                elses = parser.parse_multi_assign(start, end, allow_type=True)
+                continue
+
+        if assigns is None:
+            raise ParserError(
+                "Set expecting expressions",
+                self.template.filename,
+                line
+            )
+
+        node = AssignNode(self.template, line, assigns, elses)
         self.parser.add_node(node)
+
 
 ACTION_HANDLERS = {
     "set": SetActionHandler,
